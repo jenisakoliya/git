267f5bf88800b860e051bf39643476d429e70cfd
jenisakoliya <ajlearn119@gmail.com>
2023-08-03 11:06:19 +0530

    adding code in diff.java

--- diff.java
+++ diff.java
@@ -1,3 +1,8 @@
-public class sum{
-    
+public class diff{
+   public static void main(String[] args)
+   {
+    int a=10;
+    int b=15;
+    System.out.println("a: " + a + " b: " + b +"a-b: " + (a -b));
+   }
 }
